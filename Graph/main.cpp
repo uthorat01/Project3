47bee6b37e5b0589aa7e413653600a64e0544c8f
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2021-12-06 12:58:57 -0500

    minor changes to runtime behavior

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,24 +1,22 @@
 #include "Graph.h"
 
 int main() {
-  Graph Wikipedia;
-  while (true) {
-	  std::string node;
-	  std::string target;
-	  std::cin >> node;
-	  std::cin >> target;
+  	Graph Wikipedia;
+	std::string node;
+	std::string target;
+	std::cin >> node;
+	std::cin >> target;
 
-	  auto t1 = std::chrono::high_resolution_clock::now();
-	  std::pair<int, std::string> iddfs = Wikipedia.IDDFS(node, target);
-	  auto t2 = std::chrono::high_resolution_clock::now();
-	  std::cout << "IDDFS run time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " ms|";
+	auto t1 = std::chrono::high_resolution_clock::now();
+	std::pair<int, std::string> iddfs = Wikipedia.IDDFS(node, target);
+	auto t2 = std::chrono::high_resolution_clock::now();
+	std::cout << "IDDFS Runtime: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " ms|";
 
-	  t1 = std::chrono::high_resolution_clock::now();
-	  int bfs = Wikipedia.BFS(node, target);
-	  t2 = std::chrono::high_resolution_clock::now();
-	  std::cout << "BFS run time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " ms|";
+	t1 = std::chrono::high_resolution_clock::now();
+	int bfs = Wikipedia.BFS(node, target);
+	t2 = std::chrono::high_resolution_clock::now();
+	std::cout << "BFS Runtime: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " ms|";
 
-	  std::cout << iddfs.first << "|" << bfs << "|" << node + " -> " << iddfs.second.substr(0, iddfs.second.size()-4);
-	  return 0;
-  }
+	std::cout << "Distance: " << iddfs.first << " pages|Distance: " << bfs << " pages|" << node + " -> " << iddfs.second.substr(0, iddfs.second.size()-4);
+  	return 0;
 }
