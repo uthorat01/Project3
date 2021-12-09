4e1fd5e4fba06dbb097af4057915f420219e833b
nick-dinh <nickdinhphone@gmail.com>
2021-12-09 03:05:26 -0500

    merged filter + scraper and added comments

--- Scraper/WebScraper/Scraper.py
+++ Scraper/WebScraper/Scraper.py
@@ -1,33 +1,37 @@
-# Chomp Chomp Swamp Wikipedia URL scraper 
-# development aided by BeautifulSoup documentation: https://beautiful-soup-4.readthedocs.io/en/latest/
+'''
+Chomp Chomp Swamp Wikipedia URL scraper 
+development aided by BeautifulSoup documentation: https://beautiful-soup-4.readthedocs.io/en/latest/
+Gets all the forward links/hyper links nested on a page
+Takes in a file consisting of Wikipedia articles and appends a list of hyper links on the end
+'''
+
 import requests
 import re
+import csv
 import json
 from bs4 import BeautifulSoup
 
-
-# article url
+'''
+Scraping
+'''
+# articles from text file
 articlename = input("input text file: ")
 article = open(articlename, "r")
 wikipedia = article.readlines()
 
-# text file name
-filename = input("output text file: ")
-file = open(filename, "a")
+# output CSV file name
+file = open("link.csv", "a")
 
 # regex to match urls: urls can only have letters, _'s, /'s, and :'s
 regex = re.compile("^[a-zA-Z/_:]*$");
-
-i = 0
+print("in progress...", end='')
 
 for wiki in wikipedia:
-    print(str(i) + " out of " + str(len(wikipedia)))
-    i+=1
     # write article url for the beginning
-    file.write(wiki[:-1])
+    file.write("https://en.wikipedia.org" + wiki[:-2])
 
     # use requests library to get html of the article & soupify it into usable data
-    page = requests.get(wiki[:-1])
+    page = requests.get("https://en.wikipedia.org" + wiki[:-2])
     data = BeautifulSoup(page.content, "html.parser")
 
     # general line format we want to grab from:
@@ -38,16 +42,65 @@ for wiki in wikipedia:
         url = link.get('href')
         # filter out the urls that dont start with '/wiki/'
         if str(url).startswith("/wiki/"):
-            # filter out urls that have special characters
-            if (regex.match(str(url)) is not None):
+            # we do not want any articles Help, Portal, Special, User, Wikipedia, Template, Main_Page, Template_Talk, Talk pages, or pages that are invalid (have invalid char or aren't a wiki page)
+            filter = ["/wiki/Help:", "/wiki/Portal:", "/wiki/Special:", "/wiki/User:", "/wiki/Wikipedia:", "/wiki/Template:", "/wiki/Main_Page", "/wiki/Template_talk:", "/wiki/Talk:"]
+            # filter out urls that have special characters aswell
+            if (regex.match(str(url)) is not None and url.startswith(tuple(filter)) is not True):
                 # filter out words that arent in the filter
                 file.write(",")
                 file.write(url)
-
+    # go next
     file.write("\n")
 
-# create new line and close CSV file
+# clean up
 article.close()
 file.close()
+
+
+'''
+Filtering:
+'''
+print('filtering...', end='')
+
+# import filter
+filterfile = open("filter.json", "r")
+filter = json.load(filterfile)
+set_filter = set(filter['filter'])
+
+# export to file:
+file = open("filtered.csv", "a")
+
+# import data and filter data
+with open("links.csv", newline='') as data:
+    # keep track of current so that we dont want to have pages link to themselves
+    current = ""
+    # read in our data using a csv reader
+    values = csv.reader(data, delimiter=",")
+    # for each row
+    for row in values:
+        # for each value in the row
+        for value in row:
+            # if its the initial page, write it but without the beginning 
+            if str(value).startswith("https"):
+                current = str(value)[24:]
+                file.write("\n")
+                file.write(value)
+                continue
+            # we dont want to have pages link to themselves
+            if value == current:
+                continue
+            # if its not a whitelisted term, delete it
+            if value not in set_filter:
+                continue
+            # it is a whitelisted term so write it to the filtered file
+            else:
+                file.write(",")
+                file.write(value)
+
+# clean up
+print("complete")
+filterfile.close()
+file.close()
+data.close()
 
 
