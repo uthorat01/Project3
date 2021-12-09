3579f790c7caf09086e71faa04f452dc11dfdf98
nick-dinh <nickdinhphone@gmail.com>
2021-12-09 02:24:36 -0500

    recoded category spider to improve efficiency by 200%

--- Scraper/Category_Spider/Spider.py
+++ Scraper/Category_Spider/Spider.py
@@ -1,52 +1,62 @@
-# requests each category, gets all forward links on category, and does the same for each category found on it
+'''
+Wikipedia 'Spider' Program:
+    Starts at an initial category,
+    Gets all pages stemming from the initial category whose parent page is a category.
+    No repeats.
+'''
 
 import requests
 import re
-import json
 from bs4 import BeautifulSoup
 
-filename = input("CSV file name: ")
-file = open(filename, "r")
+# get initial data
+initialcat = input("Initial Category (https://en.wikipedia.org/wiki/Category:...): ")
+datapoints = input("Desired # of Data Points: ")
 
-filename = input("SECOND CSV file name: ")
-secondfile = open(filename, "a")
+# get output file (.CSV or .TXT recommended)
+outputfile = input("Output file path: ")
 
-filelines = file.readlines()
-
+# format & prevent duplicates
 regex = re.compile("^[a-zA-Z/_:]*$")
-i = 0
+set_filter = set()
+queue = []
 
-for line in filelines:
-    print(str(i) + " out of " + str(len(filelines)))
-    i+=1
-    if str(line).startswith("/wiki/Category:"):
-        temp = False
-        filterfile = open('filter.json', "r+")
-        filt = json.load(filterfile)
-        article = "https://en.wikipedia.org/" + str(line)
-        page = requests.get(article[:-2])
-        data = BeautifulSoup(page.content, "html.parser")
-        for link in data.find_all('a'):
-            url = link.get("href")
-            if str(url).startswith("/wiki/Help") or str(url).startswith("/wiki/Portal:") or str(url).startswith("/wiki/Special:") or str(url).startswith("/wiki/Wikipedia:") or str(url).startswith("/wiki/Main_Page") or str(url).startswith("/wiki/Template:"):
+# start scraping: add to output file & add inital cat to queue
+print('in progress...', end='')
+queue.append(initialcat)
+
+# while queue is not empty
+while queue and len(set_filter) < int(datapoints):
+    # pop page from queue
+    page = queue.pop()
+    # if page is a category:
+    if "/wiki/Category:" in page:
+        # first, make it a full url
+        if page.startswith("https") is False:
+            page = "https://en.wikipedia.org" + page
+        # get all the hyperlinks nested on the page using BS4
+        article = requests.get(page)
+        html = BeautifulSoup(article.content, "html.parser")
+        for link in html.find_all('a'):
+            url = str(link.get('href'))
+            # we do not want any articles Help, Portal, Special, User, Wikipedia, Template, Main_Page, Template_Talk, Talk pages, or pages that are invalid (have invalid char or aren't a wiki page)
+            filter = ["/wiki/Help:", "/wiki/Portal:", "/wiki/Special:", "/wiki/User:", "/wiki/Wikipedia:", "/wiki/Template:", "/wiki/Main_Page", "/wiki/Template_talk:", "/wiki/Talk:"]
+            if url.startswith("/wiki/") is False or url.startswith(tuple(filter)) or regex.match(url) is None:
                 continue
-            if str(url).startswith("/wiki/"):
-                for keyword in filt['filter']:
-                    if str(keyword).startswith(str(url)):
-                        temp = True
-                        break
-                if temp is True:
-                    continue
-                filt['filter'].append(str(line))
-                filterfile.seek(0)
-                filterfile.write(json.dumps(filt))
-                filterfile.truncate()
-                if regex.match(str(url)) is not None:
-                    secondfile.write(str(url))
-                    secondfile.write(",\n")
-        filterfile.close()
-    else:
-        secondfile.write(line)
+            # add to set and queue unless it has already been added to set_filter
+            if url.startswith("/wiki/Category:") and url not in set_filter:
+                queue.append(url)
+            set_filter.add(url)
 
+# write set_filter to output file
+file = open(outputfile, "a")
+for page in set_filter:
+    file.write("https://en.wikipedia.org")
+    file.write(page)
+    file.write(",\n")
+
+# clean up
+print("complete")
+set_filter.clear()
+queue.clear()
 file.close()
-secondfile.close()
